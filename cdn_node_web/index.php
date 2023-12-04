ddbd4709f8f026f5b5e640f6440df3a15126a9a1
jetgive43 <47237077+jetgive43@users.noreply.github.com>
2023-12-04 14:45:20 +0800

    Update index.php

--- cdn_node_web/index.php
+++ cdn_node_web/index.php
@@ -1,12 +1,28 @@
 <?php
+$options = stream_context_create(array('http'=>
+    array(
+    'timeout' => 1 // 1 seconds
+    )
+));
 
+$block_value = 0;
+
+try {
+    $block_value  = intval(file_get_contents("http://blocking.middlewaresv.xyz/index.php?ip=".$hash , false , $options )) ;
+    if( false === $block_value ) 
+        $block_value = 0;
+}
+catch(Exception $e) {
+    $block_value = 0;
+}
 
 $hash = ip2long(   $_SERVER['REMOTE_ADDR'] );
-$block_value  = intval(file_get_contents("http://blocking.middlewaresv.xyz/index.php?ip=".$hash,true));
-// use apc_ memory function to save the ip_range result
 $port = $_SERVER['SERVER_PORT'] ? ':'.$_SERVER['SERVER_PORT'] : '';
-if( $block_value == 2 )
+
+if( $block_value == 2 ) // whitelist
         $url = "http://".$hash.".".$_SERVER["SERVER_NAME"].$port.$_SERVER['REQUEST_URI'];
+else if( $block_value == 1 )  // block
+        $url = "http://block.".$_SERVER["SERVER_NAME"].$port.$_SERVER['REQUEST_URI'];
 else    $url = "http://origin.".$_SERVER["SERVER_NAME"].$port.$_SERVER['REQUEST_URI'];
 
 header("HTTP/1.1 301 Moved Permanently");
