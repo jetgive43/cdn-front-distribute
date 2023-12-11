01644c104f0cb38a069704bc7ee11ade99b05132
jetgive43 <47237077+jetgive43@users.noreply.github.com>
2023-12-12 00:11:51 +0800

    Update index.php

--- cdn_node_web/index.php
+++ cdn_node_web/index.php
@@ -8,29 +8,20 @@ $hash = ip2long(   $_SERVER['REMOTE_ADDR'] );
 $block_value = 0;
 
 try {
-    $res  = intval(file_get_contents("http://blocking.middlewaresv.xyz/api/blockedip/check_ext?ip=".$hash , false , $options )) ;
-    $R=explode( ":" , $res );
-    $block_state =  $res[0];
-    $country_code =  $res[1];
-    $token =  $res[2];
-    if( false === $block_value ){
-        $block_state = 0;
-        $country_code = "DE";
-        $token = 11111;
-    }
+    $block_value  = intval(file_get_contents("http://blocking.middlewaresv.xyz/api/blockedip/check?ip=".$hash , false , $options )) ;
+    if( false === $block_value ) 
+        $block_value = 0;
 }
 catch(Exception $e) {
-    $block_state = 0;
-    $country_code = "DE";
-    $token = 11111;
+    $block_value = 0;
 }
+
 
 $port = $_SERVER['SERVER_PORT'] ? ':'.$_SERVER['SERVER_PORT'] : '';
 
-if( $block_state == 2 ){ // whitelist
-        $url = "http://".$country_code."-".$token.".".$_SERVER["SERVER_NAME"].$port.$_SERVER['REQUEST_URI'];
-}
-else if( $block_state == 1 )  // block
+if( $block_value == 2 ) // whitelist
+        $url = "http://front.".$_SERVER["SERVER_NAME"].$port.$_SERVER['REQUEST_URI'];
+else if( $block_value == 1 )  // block
         $url = "http://block.".$_SERVER["SERVER_NAME"].$port.$_SERVER['REQUEST_URI'];
 else    $url = "http://origin.".$_SERVER["SERVER_NAME"].$port.$_SERVER['REQUEST_URI'];
 
