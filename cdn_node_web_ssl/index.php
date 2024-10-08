c10343e91ad20fc51d06ece6821dcc8622de5b44
jetgive43 <47237077+jetgive43@users.noreply.github.com>
2024-10-08 20:07:21 +0800

    Update index.php

--- cdn_node_web_ssl/index.php
+++ cdn_node_web_ssl/index.php
@@ -129,5 +129,6 @@ if( $block_value == 1 ) { // block
 
 // Redirect to the appropriate URL
 // echo $block_value
+header('Access-Control-Allow-Origin: *');
 header("Location: $url", true, 302);
 ?>
