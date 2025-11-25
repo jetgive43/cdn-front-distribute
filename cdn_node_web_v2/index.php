d00b0c92bafa7d228ec2cdb13933e2e53c75bd0b
hserver71 <sprite12131@outlook.com>
2025-11-25 16:57:59 +0900

    Refactor URL generation logic for CDN handling

--- cdn_node_web_v2/index.php
+++ cdn_node_web_v2/index.php
@@ -4,8 +4,6 @@ $ipv4 = filter_var($_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP,FILTER_FLAG_IPV4)
 $is_ipv4 = ( $ipv4 == $_SERVER['REMOTE_ADDR'] );
 $ip_hash = ip2long($_SERVER['REMOTE_ADDR']);
 
-
-
 try {
     // Server_name  *.xxx.com
     // HTTP_HOST  user_requested.xxx.com
@@ -36,7 +34,6 @@ function binarySearch($data, $ip) {
 
     while ($low <= $high) {
         $mid = (int)(($low + $high) / 2);
-
         if ($ip < $data[$mid]['start']) {
             $high = $mid - 1;
         } elseif ($ip > $data[$mid]['end']) {
@@ -114,17 +111,29 @@ $subDNS = explode(".", $_SERVER["HTTP_HOST"], 2)[0];
 $masterDNS = explode(".", $domain["stream_dns_name"], 2)[1];
 $blackhole_domains = json_decode(apcu_fetch('blackhole_domains'), true);
 
-if ($block_value == 1) { 
-    $url = "http://block-" . ip2long($domain["ip"]) . "." . $blackhole_domains[array_rand($blackhole_domains)]["name"]. $_SERVER['REQUEST_URI'];
-} else if ($block_value == 0 && $dns_country_enabled == 1) { // not blocked ip and and backnode is blocked from client's country
-    if( $wildcard_flag ){
-        $url = "http://".$country_code."-" . $subDNS . "." . $masterDNS . $_SERVER['REQUEST_URI']; //http://xx-jwalt-1.treelive.ink/index2.php
+$use_cf_cdn = $domain["cf_cdn_list"] != null && strlen($domain["cf_cdn_list"]) > 1 && strpos($domain["cf_cdn_list"], strtoupper($country_code)) !== false;
+$cf_dns_list = json_decode(apcu_fetch(strtolower($domain["ip"])), true);
+$random_dns = $cf_dns_list[array_rand($cf_dns_list)];
+
+
+if($use_cf_cdn && $random_dns ) {
+    $url = "http://" . $random_dns["record"].".".$random_dns["domain_name"] . $_SERVER['REQUEST_URI'];
+}else{
+    if ($block_value == 1) { 
+        $url = "http://block-" . ip2long($domain["ip"]) . "." . $blackhole_domains[array_rand($blackhole_domains)]["name"]. $_SERVER['REQUEST_URI'];
+    } else if ($block_value == 0 && $dns_country_enabled == 1) { // not blocked ip and and backnode is blocked from client's country
+        if( $wildcard_flag ){
+            $url = "http://".$country_code."-" . $subDNS . "." . $masterDNS . $_SERVER['REQUEST_URI']; //http://xx-jwalt-1.treelive.ink/index2.php
+        } else {
+            $url = "http://" . $subDNS . "." . $masterDNS . $_SERVER['REQUEST_URI'];
+        }
     } else {
-        $url = "http://" . $subDNS . "." . $masterDNS . $_SERVER['REQUEST_URI'];
+        $url = "http://" . $domain["ip"] . $_SERVER['REQUEST_URI'];
     }
-} else {
-    $url = "http://" . $domain["ip"] . $_SERVER['REQUEST_URI'];
 }
+
+
+
 // Redirect to the appropriate URL
 header('Content-Type: text/html; charset=UTF-8');
 header('Access-Control-Allow-Origin: *'); 
