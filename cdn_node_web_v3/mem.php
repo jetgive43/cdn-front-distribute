a4eeefff5ef78f87277a227416fbcd80b5552990
hserver71 <sprite12131@outlook.com>
2026-01-21 08:59:58 +0900

    Update mem.php

--- cdn_node_web_v3/mem.php
+++ cdn_node_web_v3/mem.php
@@ -1,19 +1,19 @@
 <?php
 function fetchAndCachePortugalBackData() {
+    // Clear all user cache entries in APCu
     global $options; 
     //save the stream back data as json structure in apcu
     $data = file_get_contents('https://dev.host-palace.net/portual_cdn_api', false, $options);
+    $cf_data = file_get_contents('https://dev.host-palace.net/get_cf_dns_list', false, $options);
+    apcu_clear_cache();
     if ($data !== false) {
         $portugal_back_data = json_decode($data, true);
         foreach ($portugal_back_data as $d) {
-            apcu_delete(strtolower($d['domain']));
             apcu_store(strtolower($d['domain']) , json_encode($d));
         }
     }
-    $cf_data = file_get_contents('https://dev.host-palace.net/get_cf_dns_list', false, $options);
     if ($cf_data !== false) {
         $cf_dns_list = json_decode($cf_data, true);
-        // save the cf dns data with the backnode as key and groupby the backnode
         $cf_dns_data_grouped = [];
         foreach ($cf_dns_list as $d) {
             $cf_dns_data_grouped[$d['backnode']][] = $d;
